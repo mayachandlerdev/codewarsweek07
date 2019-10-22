19f4666baba9e69cf2af29c80e9e11c5c9f7bf1a
Maya Chandler <55395525+mayachandlerdev@users.noreply.github.com>
2019-10-22 15:28:20 -0400

    Add files via upload

---
+++ codeWarsWeek07(1).js
@@ -0,0 +1,6 @@
+//We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+var stringToNumber = function(str){
+ 	return Number(str);
+ }
+ stringToNumber (6)
